5db19ee334076e12b63c84cf3407091f567bec5a
Ayanzha <ayanzha8991@gmail.com>
2022-10-04 21:54:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# from experta import *
+# import Rule
+# engine = Rule.CLOTHES()
+# engine.reset()
+#
+# felid1=input("enter your age")
+# felid2=input("enter your Skin Color")
+# felid3=input("enter your Occasion")
+# felid4=input("enter your Gender")
+# felid5=input("enter your Person_type")
+# felid6=input("enter your Season")
+# felid7=input("enter your piece_name")
+# felid8=input("enter your color_name")
+# felid9=input("enter your Person_type")
+#
+# engine.declare(Rule.age(value=int(felid1)) ,Rule.SkinColor(color=felid2),Rule.Occasion(felid3),Rule.Gender(felid4),Rule.Person_type(felid5),Rule.Season(felid6),Rule.piece_name(felid7),Rule.piece_color(felid8),Rule.Person_type(felid9))
+# engine.facts
+# engine.run()
+# print(engine.result)
+# print(engine.myclothes)
+# print(engine.myclothes.colorShirt)
